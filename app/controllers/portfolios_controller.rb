fde692ac1f42b3c3991317e65e6a8e5b04e0ae24
santosjn <jeansjcsp@gmail.com>
2018-05-23 20:11:02 -0300

    Implementing show funcionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -9,7 +9,7 @@ class PortfoliosController < ApplicationController
 	end
 
 	def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+      @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
 	    respond_to do |format|
 	      if @portfolio_item.save
@@ -23,21 +23,25 @@ class PortfoliosController < ApplicationController
 	end
 
 	def edit
-		@portfolio_item = Portfolio.find(params[:id])		
+	   @portfolio_item = Portfolio.find(params[:id])		
 	end
 
 	def update
-	@portfolio_item = Portfolio.find(params[:id])
-	
-    respond_to do |format|
-      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
-        format.html { redirect_to portfolios_path, notice: 'Your portfolio item was updated.' }
-        # format.json { render :show, status: :ok, location: @blog }
-      else
-        format.html { render :edit }
-        # format.json { render json: @blog.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+	   @portfolio_item = Portfolio.find(params[:id])
+
+	   respond_to do |format|
+	     if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+	       format.html { redirect_to portfolios_path, notice: 'Your portfolio item was updated.' }
+	       # format.json { render :show, status: :ok, location: @blog }
+	     else
+	       format.html { render :edit }
+	       # format.json { render json: @blog.errors, status: :unprocessable_entity }
+	     end
+	   end
+	end
+
+	def show
+		@portfolio_item = Portfolio.find(params[:id])
+	end
 
 end
